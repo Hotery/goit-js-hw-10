9989cfba2d5a76d210be2088965ff702054164b7
Hotery <143900154+Hotery@users.noreply.github.com>
2024-02-06 19:17:52 +0100

    goit-js-hw-10

---
+++ src/js/1-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startButton = document.querySelector('[data-start]');
+const dateInput = document.getElementById('datetime-picker');
+const countdownDisplay = document.querySelector('.timer');
+
+startButton.disabled = true;
+
+flatpickr(dateInput, {
+    allowInput: true,
+    disableMobile: true,
+    defaultDate: new Date(),
+    enableTime: true,
+    minuteIncrement: 1,
+    time_24hr: true,
+
+
+  onChange: function (selectedDates, dateStr) {
+    const selectedDate = new Date(dateStr);
+    const now = new Date();
+
+    if (selectedDate > now) {
+      startButton.disabled = false;
+      iziToast.success({
+        title: 'Success',
+      });
+    } else {
+      startButton.disabled = true;
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+    }
+  },
+});
+
+startButton.addEventListener('click', () => {
+  const selectedDate = new Date(dateInput.value);
+  const countdownInterval = setInterval(updateCountdown, 1000);
+
+  function updateCountdown() {
+    const now = new Date();
+    const timeLeft = selectedDate - now;
+
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      countdownDisplay.textContent = '00:00:00:00';
+      iziToast.info({
+        title: 'Info',
+        message: 'Countdown timer has ended!',
+      });
+    } else {
+      const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      countdownDisplay.textContent = `${formatTime(days)}:${formatTime(
+        hours
+      )}:${formatTime(minutes)}:${formatTime(seconds)}`;
+    }
+  }
+
+  function formatTime(time) {
+    return time.toString().padStart(2, '0');
+  }
+});
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', createPromises);
+
+function createPromises(event) {
+    event.preventDefault();
+
+    const delayInput = event.target.elements.delay;
+    const stateInput = event.target.elements.state;
+
+    const delayValue = parseInt(delayInput.value, 10);
+    const stateValue = stateInput.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (stateValue === 'fulfilled') {
+                resolve(`✅ Вдало виконаний проміс за ${delayValue} мс`);
+            } else {
+                reject(`❌ Невдало виконаний проміс за ${delayValue} мс`);
+            }
+        }, delayValue);
+    });
+
+    promise
+        .then((result) => {
+            console.log(result);
+            iziToast.success({
+                title: 'Успіх',
+                message: result,
+            });
+            delayInput.value = '';
+            stateInput.value = '';
+        })
+        .catch((error) => {
+            console.error(error);
+            iziToast.error({
+                title: 'Помилка',
+                message: error,
+            });
+            delayInput.value = '';
+            stateInput.value = '';
+        });
+};
